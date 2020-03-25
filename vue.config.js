98d40b068f82f5c6bcc6ed2e0a5b5ca4b1085dfd
iuhh <me@iuhh.dev>
2020-03-25 16:27:17 +0800

    feat: initial checkin for vue project template

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // publicPath: '/',
+  // devServer: {
+  //   https: true,
+  //   proxy: {
+  //     '^/api': {
+  //       target: 'http://192.168.5.1:8080',
+  //       changeOrigin: true
+  //     }
+  //   }
+  // },
+  transpileDependencies: ['vuetify']
+};
